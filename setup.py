a6c52b6d9ca6f707d29aee5b70bd090860359853
discodyer <cody23333@gmail.com>
2023-05-29 02:34:46 +0800

    🥨init setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ACLink",  # 包的名称
+    version="1.0",  # 包的版本
+    author="Cody Gu",
+    author_email="cody23333@gmail.com",
+    description="Communication library for ACFly A9",  # 简单描述
+    url="https://opendrone.tech/",  # 项目主页
+    packages=find_packages(),  # 需要打包的模块
+    install_requires=[
+        "docutils>=0.3",
+        "",
+        "",
+    ],  # 依赖的其他包
+    classifiers=[  # 分类信息
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+)
